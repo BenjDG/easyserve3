1659908f0ddb4d2833b16aee0090aa8f0b7fbf2a
Ben Galloway <bdgalloway85@gmail.com>
2021-02-11 14:32:17 -0600

    old server working

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,7 +1,9 @@
 const router = require('express').Router();
 const apiRoutes = require('./api');
+const htmlRoutes = require('./html');
 
-// API Routes
+// api routes
 router.use('/api', apiRoutes);
+router.use('/', htmlRoutes);
 
 module.exports = router;
