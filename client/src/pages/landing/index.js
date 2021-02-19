481d0b1aa1e0cb3f9c9376d9d0fc946e357a9006
Ben Galloway <bdgalloway85@gmail.com>
2021-02-19 02:49:33 -0600

    add react test page

--- client/src/pages/landing/index.js
+++ client/src/pages/landing/index.js
@@ -7,23 +7,22 @@ import { Grid } from '@material-ui/core';
 const useStyles = makeStyles((theme) => ({
   root: {
     '& > * + *': {
-      marginLeft: theme.spacing(2),
+      marginLeft: theme.spacing(2)
     }
   }
 }));
 
 function Landing () {
   const classes = useStyles();
-  const preventDefault = (event) => event.preventDefault();
   return (
     <Grid container>
       <Grid item xs={2} />
       <Grid item xs={8}>
         <Typography className={classes.root}>
-          <Link href='/test' onClick={preventDefault} color='inherit'>
+          <Link href='/' color='inherit'>
             Home
           </Link>
-          <Link href='/test' onClick={preventDefault} color='inherit'>
+          <Link href='/test' color='inherit'>
             McTesty
           </Link>
         </Typography>
