230ac4a07f142ab6d6eaee773d5108a634904023
Ben Galloway <bdgalloway85@gmail.com>
2021-02-19 01:20:07 -0600

    setup route folders

--- server/routes/api/index.js
+++ server/routes/api/index.js
@@ -1,6 +1,10 @@
 const router = require('express').Router();
 const authRoutes = require('./auth');
+const empRoutes = require('./emp');
+const menuitem = require('./menuItem');
 
 router.use('/auth', authRoutes);
+router.use('/emp', empRoutes);
+router.use('/menuitems', menuitem);
 
 module.exports = router;
