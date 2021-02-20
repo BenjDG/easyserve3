cc8ec681b62b117e6a6f1a9404a272311d346ea3
Myles Burkemper <myles.burkemper@gmail.com>
2021-02-20 14:34:02 -0600

    updated front end to have all item pull from db for mvp

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,47 @@
-import React from 'react';
-import { BrowserRouter, Route } from 'react-router-dom';
-import { createMuiTheme, ThemeProvider } from '@material-ui/core/styles';
-import Landing from './pages/Landing';
-import McTestFace from './McTesty/McTestFace';
+import React from "react";
+import { BrowserRouter, Route } from "react-router-dom";
+import { createMuiTheme, ThemeProvider } from "@material-ui/core/styles";
+import Landing from "./pages/Landing";
+import TableStatus from "./pages/TableStatus";
+import MainMenu from "./pages/MainMenu";
+import HotDogMenu from "./pages/HotDogMenu";
+import IceCreamMenu from "./pages/IceCreamMenu";
+import SidesMenu from "./pages/SidesMenu";
+import DrinkMenu from "./pages/DrinkMenu";
+import McTestFace from "./McTesty/McTestFace";
 
 const theme = createMuiTheme({
-  spacing: 12
+  spacing: 12,
 });
 
-function App () {
+function App() {
   return (
     <div>
       <ThemeProvider theme={theme}>
         <BrowserRouter>
-          <Route exact strict path='/'>
+          <Route exact strict path="/">
             <Landing />
           </Route>
-          <Route exact strict path='/test'>
+          <Route exact strict path="/test">
             <McTestFace />
+          </Route>
+          <Route exact strict path="/tablestatus">
+            <TableStatus />
+          </Route>
+          <Route exact strict path="/mainmenu">
+            <MainMenu />
+          </Route>
+          <Route exact strict path="/hotdogs">
+            <HotDogMenu />
+          </Route>
+          <Route exact strict path="/sides">
+            <SidesMenu />
+          </Route>
+          <Route exact strict path="/icecream">
+            <IceCreamMenu />
+          </Route>
+          <Route exact strict path="/drinks">
+            <DrinkMenu />
           </Route>
         </BrowserRouter>
       </ThemeProvider>
--- client/src/pages/mainMenu/index.js
+++ client/src/pages/mainMenu/index.js
@@ -1,1 +1,51 @@
-/* Placeholder */
+import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import Link from "@material-ui/core/Link";
+import Typography from "@material-ui/core/Typography";
+import { Grid } from "@material-ui/core";
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    "& > * + *": {
+      marginLeft: theme.spacing(2),
+    },
+  },
+}));
+
+function MainMenu() {
+  const classes = useStyles();
+  return (
+    <Grid container>
+      <Grid item xs={2} />
+      <Grid item xs={8}>
+        <Typography className={classes.root}>
+          <ul>
+            <li>
+              <Link href="/hotdogs" color="inherit">
+                Hot Dogs
+              </Link>
+            </li>
+            <li>
+              <Link href="/sides" color="inherit">
+                Sides
+              </Link>
+            </li>
+            <li>
+              <Link href="/drinks" color="inherit">
+                Drinks
+              </Link>
+            </li>
+            <li>
+              <Link href="/icecream" color="inherit">
+                Ice Cream
+              </Link>
+            </li>
+          </ul>
+        </Typography>
+      </Grid>
+      <Grid item xs={2} />
+    </Grid>
+  );
+}
+
+export default MainMenu;
