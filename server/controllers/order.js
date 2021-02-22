d537910b3ccb74ef7a28c90f59897173a72e898c
Ben Galloway <bdgalloway85@gmail.com>
2021-02-21 23:20:59 -0600

    remove space from a file

--- server/controllers/order.js
+++ server/controllers/order.js
@@ -11,7 +11,6 @@ module.exports = {
       .then(result => res.json(result))
       .catch(err => res.status(500).json(err));
   },
-  // start here 02/20 ##################################
   findOrderByIdWithItems: function (req, res) {
     db.order.findAll({
       include: [
