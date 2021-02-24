ab8f34a79f45fbd27e8defc10c8d2206d461f34d
Ben Galloway <bdgalloway85@gmail.com>
2021-02-23 21:04:57 -0600

    directory case sensitivity

---
+++ client/src/pages/TableStatus/index.js
@@ -0,0 +1,54 @@
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import Link from '@material-ui/core/Link';
+import Typography from '@material-ui/core/Typography';
+import { Grid } from '@material-ui/core';
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    '& > * + *': {
+      marginLeft: theme.spacing(2)
+    }
+  }
+}));
+
+function TableStatus () {
+  const classes = useStyles();
+  return (
+    <Grid container>
+      <Grid item xs={2} />
+      <Grid item xs={8}>
+        <Typography className={classes.root}>
+          <Link href='/' color='inherit'>
+            Home
+          </Link>
+          <ul>
+            <li>
+              <Link href='/MainMenu' color='inherit'>
+                Table 1
+              </Link>
+            </li>
+            <li>
+              <Link href='/MainMenu' color='inherit'>
+                Table 2
+              </Link>
+            </li>
+            <li>
+              <Link href='/MainMenu' color='inherit'>
+                Table 3
+              </Link>
+            </li>
+            <li>
+              <Link href='/MainMenu' color='inherit'>
+                Table 4
+              </Link>
+            </li>
+          </ul>
+        </Typography>
+      </Grid>
+      <Grid item xs={2} />
+    </Grid>
+  );
+}
+
+export default TableStatus;
