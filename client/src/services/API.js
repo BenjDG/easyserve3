78b483115dfae66cf2a6a48c798efb5ea8bbc8f7
Ben Galloway <bdgalloway85@gmail.com>
2021-02-24 00:50:20 -0600

    add order routes to front end

--- client/src/services/API.js
+++ client/src/services/API.js
@@ -1,9 +1,6 @@
 import axios from 'axios';
 
 export default {
-  getEmployees: function () {
-    return axios.get('/api/emp');
-  },
   getHotdogs: function () {
     return axios.get('/api/menuitems/hotdogs');
   },
@@ -15,5 +12,44 @@ export default {
   },
   getIcecreams: function () {
     return axios.get('/api/menuitems/icecreams');
+  },
+  findAllEmployees: function () {
+    return axios.get('/api/emp');
+  },
+  findEmployeeById: function () {
+    return axios.get('/api/emp/:id');
+  },
+  createNewEmployee: function () {
+    return axios.post('/api/emp/create');
+  },
+  updateEmployee: function () {
+    return axios.put('/api/emp/update');
+  },
+  deleteEmployee: function () {
+    return axios.delete('/api/emp/delete');
+  },
+  findAllOrders: function () {
+    return axios.get('/api/order/all');
+  },
+  findOrderById: function () {
+    return axios.get('/api/order/:id');
+  },
+  findOrderByIdWithItems: function () {
+    return axios.get('/api/order/:id/items');
+  },
+  createNewOrder: function () {
+    return axios.post('/api/order/create');
+  },
+  createNewOrderItem: function () {
+    return axios.post('/api/order/add/:orderId/:itemId');
+  },
+  updateOrderInfo: function () {
+    return axios.put('/api/order/update');
+  },
+  deleteOneOrderItem: function () {
+    return axios.delete('/api/order/delete/item/:orderItemRecId');
+  },
+  deleteOneOrder: function () {
+    return axios.delete('/api/order/delete');
   }
 };
