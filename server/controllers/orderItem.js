fa90a681fb26dec14a400602003b6e15e6e3eecb
Ben Galloway <bdgalloway85@gmail.com>
2021-02-23 23:29:37 -0600

    add orderItem controler

--- server/controllers/orderItem.js
+++ server/controllers/orderItem.js
@@ -1,8 +1,13 @@
 const db = require('../models');
 
 module.exports = {
-  createOrderItem: function (_req, res) {
-    db.orderItem.findAll({})
+  createNewOrderItem: async function (req, res) {
+    // example { empId: '2', tableId: '2', statusId: '2', notes: 'I like tacos' }
+    const { orderId, itemId } = req.params;
+    db.orderItem.create({
+      order_numberId: orderId,
+      menu_itemId: itemId
+    })
       .then(result => res.json(result))
       .catch(err => res.status(500).json(err));
   }
--- server/routes/api/order/index.js
+++ server/routes/api/order/index.js
@@ -1,10 +1,12 @@
 const router = require('express').Router();
 const order = require('../../../controllers/order');
+const orderItem = require('../../../controllers/orderItem');
 
 // route path 'api/order/'
 router.route('/all').get(order.findAllOrders);
 router.route('/:id').get(order.findOrderById);
 router.route('/:id/items').get(order.findOrderByIdWithItems);
 router.route('/create').post(order.createNewOrder);
+router.route('/add/:orderId/:itemId').post(orderItem.createNewOrderItem);
 
 module.exports = router;
