4c3d192fb6c95512532d35a282683e54a6e9a067
Ben Galloway <bdgalloway85@gmail.com>
2021-02-23 22:41:29 -0600

    folder name fix 2

--- client/src/App.js
+++ client/src/App.js
@@ -10,7 +10,7 @@ import SidesMenu from './pages/sidesMenu';
 import DrinkMenu from './pages/drinkMenu';
 import McTestFace from './mcTesty/mcTestFace';
 import Tables from './pages/tables';
-import Nav from './components/nav';
+import Nav from './components/Navttt';
 
 const theme = createMuiTheme({
   spacing: 12
