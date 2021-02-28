4b13350c37cca016b65baae1f1c8eeb874e42205
Darin Wallace <dwalleye66@gmail.com>
2021-02-28 16:09:49 -0600

    needs styling

--- client/src/pages/viewOrders/index.js
+++ client/src/pages/viewOrders/index.js
@@ -1,18 +1,39 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import Typography from '@material-ui/core/Typography';
 import { Box, Grid } from '@material-ui/core';
+import API from '../../services/API';
 
 const useStyles = makeStyles((theme) => ({
-  root: {
-    '& > * + *': {
-      marginLeft: theme.spacing(2)
-    }
+  orderView: {
+    padding: theme.spacing(2, 2),
+    height: 200,
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'flex-start'
   }
 }));
 
 function ViewOrders () {
   const classes = useStyles();
+  const [viewOrders, setAllOrders] = useState([])
+
+  useEffect(() => {
+    loadData();
+  }, []);
+
+  const loadData = () => {
+    API.findAllOrders()
+      .then((res) => {
+        setAllOrders(res.data);
+      })
+      .catch((err) => {
+        console.error(err);
+        // const error = new Error(err);
+        // setError(error.message + ' - Please login');
+      });
+  };
+  
   return (
     <Grid container>
       <Grid item xs={2} />
