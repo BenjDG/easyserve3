bdeea9baec7af8f1df80574af76384579eca17af
Myles Burkemper <myles.burkemper@gmail.com>
2021-03-01 21:02:46 -0600

    another lint fix

--- client/src/components/nav/index.js
+++ client/src/components/nav/index.js
@@ -1,49 +1,49 @@
-import React from "react";
-import { AppBar, Grid, Link, Toolbar } from "@material-ui/core";
+import React from 'react';
+import { AppBar, Grid, Link, Toolbar } from '@material-ui/core';
 
-function Nav() {
+function Nav () {
   return (
     <div>
-      <AppBar position="sticky">
+      <AppBar position='sticky'>
         <Toolbar>
           <Grid container>
             <Grid item xs={2} />
             <Grid
               item
               container
-              direction="row"
-              justify="space-evenly"
-              alignItems="center"
+              direction='row'
+              justify='space-evenly'
+              alignItems='center'
               xs={8}
             >
-              <Link href="/" color="inherit">
+              <Link href='/' color='inherit'>
                 Home
               </Link>
-              <Link href="/login" color="inherit">
+              <Link href='/login' color='inherit'>
                 Login
               </Link>
-              <Link href="/mainmenu" color="inherit">
+              <Link href='/mainmenu' color='inherit'>
                 Main Menu
               </Link>
-              <Link href="/neworder" color="inherit">
+              <Link href='/neworder' color='inherit'>
                 New Order
               </Link>
-              <Link href="/vieworders" color="inherit">
+              <Link href='/vieworders' color='inherit'>
                 View Orders
               </Link>
-              <Link href="/tables" color="inherit">
+              <Link href='/tables' color='inherit'>
                 Tables
               </Link>
-              <Link href="/hotdogs" color="inherit">
+              <Link href='/hotdogs' color='inherit'>
                 Hotdogs
               </Link>
-              <Link href="/sides" color="inherit">
+              <Link href='/sides' color='inherit'>
                 Sides
               </Link>
-              <Link href="/drinks" color="inherit">
+              <Link href='/drinks' color='inherit'>
                 Drinks
               </Link>
-              <Link href="/icecream" color="inherit">
+              <Link href='/icecream' color='inherit'>
                 Ice Cream
               </Link>
             </Grid>
