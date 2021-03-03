e1b5e21b91f69738356a4bef8f57433aebc364ca
Donovan Chavez <chavezdon17@gmail.com>
2021-03-02 21:06:38 -0600

    route fix

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,12 +1,7 @@
 const router = require('express').Router();
 const apiRoutes = require('./api');
-const menuItem = require('../controllers/menuItem');
 
 // api routes
 router.use('/api', apiRoutes);
-
-router.get('/test', function (req, res) {
-  console.log(menuItem.findAllHotdogs());
-});
 
 module.exports = router;
