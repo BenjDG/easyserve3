8d2f8f9afffda87fe180f51213629a23a757ea22
Donovan Chavez <chavezdon17@gmail.com>
2021-03-04 00:58:16 -0600

    lint:fix

--- client/src/components/nav/index.js
+++ client/src/components/nav/index.js
@@ -23,14 +23,38 @@ function Nav () {
               alignItems='center'
               xs={8}
             >
-              <Link component={RouterLink} to='/' color='inherit'> Home </Link>
-              <Link component={RouterLink} to='/login' color='inherit'> Login </Link>
-              <Link component={RouterLink} to='/mainmenu' color='inherit'> Mainmenu </Link>
-              <Link component={RouterLink} to='/neworder' color='inherit'> New Order </Link>
-              <Link component={RouterLink} to='/currentorder' color='inherit'> Current Order </Link>
-              <Link component={RouterLink} to='/vieworders' color='inherit'> View All Orders </Link>
-              <Link component={RouterLink} to='/tables' color='inherit'> Tables </Link>
-              <Link onClick={logout} color='inherit'> Logout </Link>
+              <Link component={RouterLink} to='/' color='inherit'>
+                {' '}
+                Home{' '}
+              </Link>
+              <Link component={RouterLink} to='/login' color='inherit'>
+                {' '}
+                Login{' '}
+              </Link>
+              <Link component={RouterLink} to='/mainmenu' color='inherit'>
+                {' '}
+                Mainmenu{' '}
+              </Link>
+              <Link component={RouterLink} to='/neworder' color='inherit'>
+                {' '}
+                New Order{' '}
+              </Link>
+              <Link component={RouterLink} to='/currentorder' color='inherit'>
+                {' '}
+                Current Order{' '}
+              </Link>
+              <Link component={RouterLink} to='/vieworders' color='inherit'>
+                {' '}
+                View All Orders{' '}
+              </Link>
+              <Link component={RouterLink} to='/tables' color='inherit'>
+                {' '}
+                Tables{' '}
+              </Link>
+              <Link onClick={logout} color='inherit'>
+                {' '}
+                Logout{' '}
+              </Link>
               {/* <Link href='/hotdogs' color='inherit'> Hotdogs </Link>
               <Link href='/sides' color='inherit'> Sides </Link>
               <Link href='/drinks' color='inherit'> Drinks </Link>
--- client/src/pages/viewOrders/index.js
+++ client/src/pages/viewOrders/index.js
@@ -1,7 +1,30 @@
 import { Box, Grid, Typography } from '@material-ui/core';
-import React, { useEffect, useState, useStyles } from 'react';
-import API from '../../services/API';
+import React, { useEffect, useState } from 'react';
+import { makeStyles } from '@material-ui/core/styles';
 
+import API from '../../services/API';
+const useStyles = makeStyles((theme) => ({
+  orderView: {
+    padding: theme.spacing(2, 2),
+    height: 200,
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'flex-start'
+  },
+  buttonView: {
+    padding: theme.spacing(2, 2)
+    // height: 200,
+    // display: 'flex',
+    // flexDirection: 'column',
+    // justifyContent: 'flex-start'
+  },
+  root: {
+    maxWidth: 345
+  },
+  media: {
+    height: 140
+  }
+}));
 function AllOrders () {
   const classes = useStyles();
   const [allorders, setAllOrders] = useState([]);
@@ -30,13 +53,19 @@ function AllOrders () {
         <Box m={2}>
           <Grid item container>
             <Grid item>
-              <Typography className={classes.root}>
-                View All Orders
-              </Typography>
+              <Typography className={classes.root}>View All Orders</Typography>
               <Grid item>
                 {allorders.map((item) => {
                   console.log(item);
-                  return <Grid item xs={3} key={item.id}>{item.id}{item.userId}{item.restTableId}{item.statusId}{item.notes}</Grid>;
+                  return (
+                    <Grid item xs={3} key={item.id}>
+                      {item.id}
+                      {item.userId}
+                      {item.restTableId}
+                      {item.statusId}
+                      {item.notes}
+                    </Grid>
+                  );
                 })}
               </Grid>
             </Grid>
