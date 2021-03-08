ec9413db6ded55b6853ebae5861f2ebb51fdc151
Ben Galloway <bdgalloway85@gmail.com>
2021-03-07 21:51:14 -0600

    style login page

--- client/src/pages/login/index.js
+++ client/src/pages/login/index.js
@@ -2,8 +2,21 @@ import React, { useState } from 'react';
 import API from '../../services/API';
 import { useHistory } from 'react-router-dom';
 import { UseUserProvider } from '../../services/userContext';
+import { Box, Button, Grid, makeStyles, TextField } from '@material-ui/core';
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    padding: theme.spacing(2, 1),
+    height: 400,
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'space-evenly',
+    alignItems: 'center'
+  }
+}));
 
 function Login () {
+  const classes = useStyles();
   const history = useHistory();
   const { setUser } = UseUserProvider();
   // Setting the component's initial login
@@ -47,36 +60,62 @@ function Login () {
   };
 
   return (
-    <div>
-      <form className='form'>
-        <input
-          value={login.email}
-          name='email'
-          onChange={handleInputChange}
-          type='email'
-          placeholder='Email'
-        />
-        <input
-          value={login.password}
-          name='password'
-          onChange={handleInputChange}
-          type='password'
-          placeholder='Password'
-        />
-        <button onClick={handleFormSubmit}>Submit</button>
-        <h2 className='error'>{err}</h2>
-      </form>
-      <h2>Demo Users:</h2>
-      <h3>Jane</h3>
-      <p>Email: 1@1.com</p>
-      <p>Password: 1234</p>
-      <h3>Billy</h3>
-      <p>Email: 1@2.com</p>
-      <p>Password: 1234</p>
-      <h3>Lloyd</h3>
-      <p>Email: 1@3.com</p>
-      <p>Password: 1234</p>
-    </div>
+    <Grid container>
+      <Grid item xs={2} />
+      <Grid item xs={8}>
+        <Box m={2}>
+          <Grid
+            container
+            className={classes.root}
+          >
+            <Grid item>
+              <TextField
+                required
+                id='outlined-email-required'
+                name='email'
+                value={login.email}
+                onChange={handleInputChange}
+                label='Email'
+                type='email'
+                variant='outlined'
+              />
+            </Grid>
+            <Grid item>
+              <TextField
+                required
+                id='outlined-password-input'
+                name='password'
+                value={login.password}
+                onChange={handleInputChange}
+                label='Password'
+                type='password'
+                autoComplete='current-password'
+                variant='outlined'
+              />
+            </Grid>
+            <Grid item>
+              <Button variant='contained' color='primary' onClick={handleFormSubmit}>
+                Submit
+              </Button>
+              <h2 className='error'>{err}</h2>
+            </Grid>
+          </Grid>
+
+        </Box>
+      </Grid>
+      <Grid item xs={2} />
+    </Grid>
+    //   <h2>Demo Users:</h2>
+    //   <h3>Jane</h3>
+    //   <p>Email: 1@1.com</p>
+    //   <p>Password: 1234</p>
+    //   <h3>Billy</h3>
+    //   <p>Email: 1@2.com</p>
+    //   <p>Password: 1234</p>
+    //   <h3>Lloyd</h3>
+    //   <p>Email: 1@3.com</p>
+    //   <p>Password: 1234</p>
+    // </div>
   );
 }
 
