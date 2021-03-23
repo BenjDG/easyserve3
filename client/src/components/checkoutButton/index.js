3f6bfbc3af4ab85712e95af0aebd1e00daef8080
Ben Galloway <bdgalloway85@gmail.com>
2021-03-22 22:58:26 -0500

    add checkout button

--- client/src/components/checkoutButton/index.js
+++ client/src/components/checkoutButton/index.js
@@ -1,9 +1,14 @@
 import { Button } from '@material-ui/core';
 import React from 'react';
+import { useHistory } from 'react-router-dom';
 import './styles.css';
 
 export default function CheckoutButton () {
+  const history = useHistory();
+  const handleClick = () => {
+    history.push('/checkout');
+  };
   return (
-    <Button className='checkout-button' variant='outlined'>Checkout</Button>
+    <Button className='checkout-button' onClick={handleClick} variant='outlined'>Checkout</Button>
   );
 }
