f4b9821f9b29f47381506bb11a156245380f55dd
Ben Galloway <bdgalloway85@gmail.com>
2021-03-29 23:31:45 -0500

    add new order button to viewOrders page

--- client/src/components/viewAllOrdersTable/index.js
+++ client/src/components/viewAllOrdersTable/index.js
@@ -1,5 +1,9 @@
-import { Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Paper, makeStyles } from '@material-ui/core';
-import React from 'react';
+import { Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Paper, makeStyles, Button } from '@material-ui/core';
+import React, { useState } from 'react';
+import { useHistory } from 'react-router';
+import { useCurrentOrderContext } from '../../services/orderContext';
+import { UseUserProvider } from '../../services/userContext';
+import API from '../../services/API';
 import ViewAllOrdersTableRow from '../viewAllOrdersTableRow';
 import './styles.css';
 
@@ -24,8 +28,29 @@ const useStyles = makeStyles((theme) => ({
 function ViewAllOrdersTable ({ allOrders, statusNamesList, userNamesList }) {
   const classes = useStyles();
   // console.log(allOrders);
+
+  const history = useHistory();
+  const [error, setError] = useState('');
+  const [_, setCurrentOrder] = useCurrentOrderContext(); // eslint-disable-line
+  // console.log(user);
+  const { user } = UseUserProvider();
+
+  const handleNewOrderClick = async () => {
+    // console.log(user);
+    await API.createNewOrder(user.id, '1', '1', null)
+      .then(async (result) => {
+        // console.log(result);
+        await setCurrentOrder(result.data.id);
+      }).then(() => history.push('/order'))
+      .catch((err) => {
+        console.error(err);
+        const error = new Error(err);
+        setError(error.message + ' - Please login');
+      });
+  };
   return (
     <div className='table-container'>
+      {error}
       {allOrders.length
         ? (
           <div>
@@ -38,7 +63,11 @@ function ViewAllOrdersTable ({ allOrders, statusNamesList, userNamesList }) {
                     <TableCell className={classes.tableCell} align='center'>Order ID</TableCell>
                     <TableCell className={classes.tableCell} align='center'>User</TableCell>
                     <TableCell className={classes.tableCell} align='center'>Status</TableCell>
-                    <TableCell className={classes.tableCell} align='center' />
+                    <TableCell className={classes.tableCell} align='center'>
+                      <Button onClick={handleNewOrderClick} variant='outlined'>
+                        New Order
+                      </Button>
+                    </TableCell>
                   </TableRow>
                 </TableHead>
                 <TableBody className={classes.tableBody}>
